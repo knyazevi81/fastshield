277e566c14b1dc4c1ce180035272369cb70319fd
Хасан Хамраев <khasanknyazev81@gmail.com>
2025-03-16 21:45:02 +0300

    create fs middleware

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
   name='fastshield',
-  version='0.0.1',
+  version='0.0.2',
   author='ilpdakz',
   author_email='khasanknyazev81@gmail.com',
   description='library to protect your fastapi applications',
@@ -16,7 +16,10 @@ setup(
   long_description_content_type='text/markdown',
   url='https://github.com/knyazevi81/fastshield',
   packages=find_packages(),
-  install_requires=['requests>=2.25.1'],
+  install_requires=[
+    'requests>=2.25.1',
+    'scikit-learn==1.5.1'
+  ],
   classifiers=[
     'Programming Language :: Python :: 3.11',
     'License :: OSI Approved :: MIT License',
